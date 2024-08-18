199a65956c83f40dd59faacdfc6f7c7340f3eef9
ElectricBudgie <lkdohse@gmail.com>
2024-08-18 19:56:50 -0400

    rename: nacho tray to nacho plate

--- src/main/java/com/electricbudge/tacosdelight/block/NachoPlateBlock.java
+++ src/main/java/com/electricbudge/tacosdelight/block/NachoPlateBlock.java
@@ -13,11 +13,11 @@ import vectorwing.farmersdelight.common.block.FeastBlock;
 
 import java.util.function.Supplier;
 
-public class NachoTrayBlock extends FeastBlock {
+public class NachoPlateBlock extends FeastBlock {
     protected static final VoxelShape PLATE_SHAPE = box(1.0D, 0.0D, 1.0D, 15.0D, 2.0D, 15.0D);
     protected static final VoxelShape PIE_SHAPE = Shapes.joinUnoptimized(PLATE_SHAPE, box(2.0D, 2.0D, 2.0D, 14.0D, 8.0D, 14.0D), BooleanOp.OR);
 
-    public NachoTrayBlock(BlockBehaviour.Properties properties, Supplier<Item> servingItem, boolean hasLeftovers) {
+    public NachoPlateBlock(BlockBehaviour.Properties properties, Supplier<Item> servingItem, boolean hasLeftovers) {
         super(properties, servingItem, hasLeftovers);
     }
 
